0ce7231cda982c41835252ae5b5a40d1e603f10e
Paing <paingthuaung353@gmail.com>
2024-02-26 20:23:33 +0630

    Remove console.log

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,3 @@
-console.log('Client js is loaded');
-
-
 const weatherForm = document.querySelector('form');
 const search      = document.querySelector('input');
 const messageOne  = document.querySelector('#message-1');
